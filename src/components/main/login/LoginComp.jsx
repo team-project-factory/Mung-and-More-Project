c7f04c635c9d42e816c12c70b8c7e66886d456a3
Kdddru <dhk9817@gmail.com>
2023-06-05 17:05:08 +0900

    0605 로그인기능 추가

--- src/components/main/login/LoginComp.jsx
+++ src/components/main/login/LoginComp.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+//css
 import style from './logincomp.module.scss'
+
+//로그인 인증
 import { auth } from '../../../data/firebase'
-import { Link } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export const LoginComp = () => {
+  const navigater = useNavigate(); 
+
+
+
+  //로그인 정보
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = (e)=>{
+    e.preventDefault();
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        navigater('/')
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
 
   return (
     <div className={style.loginList}>
@@ -13,10 +42,20 @@ export const LoginComp = () => {
           <ul>
             <h1>LOGO</h1>
             <h2>한줄 소개가 들어갈 공간</h2>
-            <form>
-              <li><input type="email" id="" placeholder='이메일(example@email.com)' required /></li>
-              <li><input type="password" id="" placeholder='비밀번호' required/></li>
-              <li><input type="submit" value={'Login'}/></li>
+            <form onSubmit={login}>
+              <li>
+                <input type="email" id="" placeholder='이메일(example@email.com)' required 
+                  onChange={(e)=>{setEmail(e.target.value)}}
+                />
+              </li>
+              <li>
+                <input type="password" id="" placeholder='비밀번호' required
+                  onChange={(e)=>{setPassword(e.target.value)}}
+                />
+              </li>
+              <li>
+                <input type="submit" value={'Login'}/>
+              </li>
             </form>
             <p>회원이 아니신가요? <Link to={'/joinmember'}>지금 가입하세요</Link> </p>
           </ul>
