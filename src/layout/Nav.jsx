9804511ca504b81e5190b770c061c3080ed61268
Kdddru <dhk9817@gmail.com>
2023-06-07 15:23:26 +0900

    0607

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 //css
 import style from './Nav.module.scss'
 
 export const Nav = () => {
-
+  //버튼 토글
+  const [btn, setBtn] = useState(false);
   return (
     <div className={style.navbar}>
       {/** 로고 */}
@@ -18,12 +19,25 @@ export const Nav = () => {
       <ul className={style.navbar_menu}>
         <li><Link to={`/shopping`}>Shopping</Link></li>
         <li><Link to={`/location`}>Loaction</Link></li>
-        <li><Link to={`/community`}>Community</Link></li>
+        <li>
+          <Link to={`/community`}>Community</Link> 
+          <span onClick={()=>{setBtn(!btn)}}>
+            {btn ? '🔽':'🔼'}
+          </span>
+          <div>
+            <div className={style.navbar_menu_comu} style={btn ? {visibility:''}: {visibility:'hidden'}}>
+              <ul className={style.navbar_menu_comu_list}>
+                <li><Link to={`/community`}>Mungstagram</Link></li>
+                <li><Link to={`/community`}>Mung's News</Link></li>
+              </ul>
+            </div>
+          </div>
+        </li>
         <li><Link to={`/notice`}>Notice</Link></li>
       </ul>
       {/** 로그인  */}
       <ul className={style.navbar_login}>
-        <li><Link to={`/login`}>Login</Link></li>
+        {}<li><Link to={`/login`}>Login</Link></li>
       </ul>
     </div>
   )
