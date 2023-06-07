0324b1b82815ec64efe866fc57621db1e1fdbe88
syzzny <son115502@gmail.com>
2023-06-07 15:32:38 +0900

    0607

--- src/components/main/login/joinmember/JoinMemberComp.jsx
+++ src/components/main/login/joinmember/JoinMemberComp.jsx
@@ -1,7 +1,292 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-export const JoinMemberComp = () => {
-  return (
-    <div>JoinMemberComp</div>
-  )
+import {
+    Wrap, JoinWrap, Jointext, LogoImage, TextWrap, StyledText, Aglog, Text, TextSpan, styleForm
+    , StyledInput, StyledFieldset, StyledLabel, StyledWrapper, StyledIconButton, StyledInputPw, PwButton,
+    CheckStyle, FormControlStyle, SubmitBtn, ErrorMsg
+} from './styles/JoinStylecomp'
+
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+const theme = createMuiTheme();
+
+export default function JoinMemberComp() {
+    const isEnglish = false;
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState(false);
+
+    const [password, setPassword] = useState('');
+
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+
+    const [name, setName] = useState('');
+    const [checked, setChecked] = React.useState([false, false, false]);
+    const [allChecked, setAllChecked] = React.useState(false);
+    const isAllChecked = checked[0] && checked[1] && checked[2];
+
+
+    // email
+    const handleInputChange = (event) => {
+        const inputValue = event.target.value;
+        const emailRegex = /^\S+@\S+\.\S+$/; // 이메일 유효성을 검사하는 정규식
+
+        setEmail(inputValue);
+
+        // 입력된 값이 이메일 형식에 맞지 않을 경우
+        if (!emailRegex.test(inputValue)) {
+            setEmailError(true);
+        }
+        // 입력된 값이 이메일 형식에 맞을 경우 이메일 에러 상태를 false로 설정
+        else {
+            setEmailError(false);
+        }
+    };
+
+    // password
+    const handleInputPw = (event) => {
+        const inputValue = event.target.value;
+
+        setPassword(inputValue);
+    }
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    // confirm password
+    const handleConfirmPasswordChange = (event) => {
+        const inputValue = event.target.value;
+
+        setConfirmPassword(inputValue);
+
+        if (inputValue !== password) {
+            setPasswordError(true);
+        } else {
+            setPasswordError(false);
+        }
+    };
+
+    const confirmPasswordErrorMessage = passwordError ? '비밀번호가 일치하지 않습니다.' : '';
+
+    // name
+    const handleInputChangeName = (event) => {
+        const inputValue = event.target.value;
+        setName(inputValue);
+    }
+
+
+    // 약관동의
+    
+
+    const handleChange1 = (event) => {
+        const isChecked = event.target.checked;
+        setChecked([isChecked, isChecked, isChecked]);
+        setAllChecked(isChecked && isChecked && isChecked);
+    };
+
+    // 회원가입 버튼
+    // 모든 입력값이 채워졌을 때 버튼을 활성화하는 상태
+    const isButtonEnabled = email && password && confirmPassword && name && !emailError && !passwordError && isAllChecked;
+
+
+    const handleChange2 = (event) => {
+        const isChecked = event.target.checked;
+        setChecked([isChecked, checked[1], checked[2]]);
+        setAllChecked(isChecked && checked[1] && checked[2]);
+    };
+
+    const handleChange3 = (event) => {
+        const isChecked = event.target.checked;
+        setChecked([checked[0], isChecked, checked[2]]);
+        setAllChecked(checked[0] && isChecked && checked[2]);
+    };
+
+    const handleChange4 = (event) => {
+        const isChecked = event.target.checked;
+        setChecked([checked[0], checked[1], isChecked]);
+        setAllChecked(checked[0] && checked[1] && isChecked);
+    };
+
+    
+
+
+    const children = (
+        <Box sx={{ display: 'flex', flexDirection: 'column', ml: 0 }}>
+            <FormControlLabel
+                label="[필수] 19세 이상이며, 서비스 이용약관에 동의합니다."
+                control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
+            />
+            <FormControlLabel
+                label="[필수] 개인정보 수집 및 이용에 동의합니다."
+                control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
+            />
+            <FormControlLabel
+                label="[선택] 00에 관한 최근 소식, 혜택 및 서비스 관련 정보를
+                Email로 받아보겠습니다."
+                control={<Checkbox checked={checked[2]} onChange={handleChange4} />}
+            />
+        </Box>
+    );
+    return (
+        <ThemeProvider theme={theme}>
+            <Wrap>
+                <JoinWrap src="../assets/img/login.png" />
+                <Jointext>
+                    <TextWrap>
+                        <LogoImage src="../assets/img/logo.svg" alt="" />
+                        <Aglog>
+                            <StyledText isEnglish={isEnglish}>회원가입</StyledText>
+                            <Text>
+                                이미 계정이 있으신가요?
+                                <TextSpan>로그인</TextSpan>
+                            </Text>
+                        </Aglog>
+                    </TextWrap>
+                    <styleForm>
+                        {/* 이메일 */}
+                        <StyledFieldset>
+                            <StyledLabel>이메일</StyledLabel>
+                            <StyledWrapper>
+                                <StyledInput
+                                    id='email'
+                                    aria-invalid='false'
+                                    autoComplete='username'
+                                    placeholder='이메일을 입력해주세요'
+                                    inputProps={{
+                                        maxLength: 60,
+                                        autoComplete: 'off',
+                                    }}
+                                    error={emailError}
+                                    variant="outlined"
+                                    helperText={emailError ? '유효한 이메일 주소를 입력해주세요' : ''}
+                                    value={email}
+                                    onChange={handleInputChange}
+                                >
+                                </StyledInput>
+                            </StyledWrapper>
+                        </StyledFieldset>
+
+                        {/* 비밀번호 */}
+                        <StyledFieldset>
+                            <StyledLabel>비밀번호</StyledLabel>
+                            <StyledWrapper>
+                                <FormControl>
+                                    <StyledInputPw
+                                        id="filled-adornment-password"
+                                        type={showPassword ? 'text' : 'password'}
+                                        placeholder='비밀번호를 입력해주세요'
+                                        value={password}
+                                        onChange={handleInputPw}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <PwButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </PwButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+                                </FormControl>
+                            </StyledWrapper>
+                        </StyledFieldset>
+
+                        {/* 비밀번호 확인 */}
+                        <StyledFieldset>
+                            <StyledLabel>비밀번호 확인</StyledLabel>
+                            <StyledWrapper>
+                                <FormControl>
+                                    <StyledInputPw
+                                        id="confirm-password"
+                                        type={showPassword ? 'text' : 'password'}
+                                        placeholder="비밀번호를 한 번 더 입력해주세요"
+                                        value={confirmPassword}
+                                        onChange={handleConfirmPasswordChange}
+                                        error={passwordError}
+                                        helperText={confirmPasswordErrorMessage}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <PwButton
+                                                    id='pwbutton2'
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </PwButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+                                </FormControl>
+                                {passwordError && (
+                                    <ErrorMsg>비밀번호가 일치하지 않습니다.</ErrorMsg>
+                                )}
+                            </StyledWrapper>
+                        </StyledFieldset>
+
+                        {/* 이름 */}
+                        <StyledFieldset>
+                            <StyledLabel>이름</StyledLabel>
+                            <StyledWrapper>
+                                <StyledInput
+                                    id='name'
+                                    aria-invalid='false'
+                                    autoComplete='username'
+                                    placeholder='이름을 입력해주세요'
+                                    inputProps={{
+                                        maxLength: 60,
+                                        autoComplete: 'off',
+                                    }}
+                                    variant="outlined"
+                                    value={name}
+                                    onChange={handleInputChangeName}
+                                >
+                                </StyledInput>
+                            </StyledWrapper>
+                        </StyledFieldset>
+
+                        {/* 약관동의 */}
+                        <StyledFieldset>
+                            <StyledLabel>약관동의</StyledLabel>
+                            <StyledWrapper>
+                                <CheckStyle>
+                                    <FormControlStyle
+                                        label="전체동의"
+                                        control={
+                                            <Checkbox
+                                                checked={isAllChecked}
+                                                onChange={handleChange1}
+                                            />
+                                        }
+                                    />
+                                    {children}
+                                </CheckStyle>
+                            </StyledWrapper>
+                        </StyledFieldset>
+
+                        <SubmitBtn
+                            enabled={isButtonEnabled}>
+                            회원가입
+                        </SubmitBtn>
+                    </styleForm>
+                </Jointext>
+            </Wrap>
+        </ThemeProvider>
+    )
 }
---
+++ src/components/main/login/joinmember/styles/JoinStylecomp.jsx
@@ -0,0 +1,195 @@
+import { CheckBox } from '@mui/icons-material';
+import { TextField, IconButton, FilledInput, FormControlLabel, Box } from '@mui/material';
+import styled from 'styled-components';
+
+export const StyledText = styled.div`
+    font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+    font-size: 1.7rem;
+    font-weight: 500;
+`;
+export const Text = styled.div`
+    font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+    font-size: 1rem;
+    color: #4D555C;
+`;
+export const TextSpan = styled.span`
+    font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+    font-size: 1rem;
+    color: #509DE0;
+    font-weight: 500;
+    cursor: pointer;
+    text-decoration: underline;
+`;
+
+export const Wrap = styled.div`
+    display: flex;
+    height: 100vh;
+`
+export const JoinWrap = styled.img`
+    /* flex: 1.3 1 auto; */
+    /* background-image: url('../assets/img/login.jpg');
+    background-size: cover; */
+    height: 100%;
+`
+export const Jointext = styled.div`
+    flex: 1;
+    padding: 82px 0;
+`
+export const LogoImage = styled.img`
+    width: 110px;
+`
+export const TextWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 35px;
+    width: 100%;
+    max-width: 360px;
+    margin: 0 auto;
+    text-align: initial;
+`
+
+export const styleForm = styled.form`
+    width: 360px;
+`
+export const Aglog = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 19px;
+`
+export const StyledFieldset = styled.div`
+    max-width: 360px;
+    margin: 0 auto;
+    margin-top: 35px;
+    margin-bottom: 35px;
+`
+export const StyledLabel = styled.label`
+    color: rgb(103,109,114);
+    display: block;
+    margin-bottom: 8px;
+    text-align: initial;
+    font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+`
+export const StyledWrapper = styled.div`
+    width: 100%;
+`
+export const StyledInput = styled(TextField)`
+    width: 360px;
+    height: 45px;
+    background-color: #F9FAFB;
+    border-radius: 5px;
+    position: relative;
+    
+    & .MuiOutlinedInput-root {
+    border: none;
+    font-size: 14px;
+    font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+    font-weight: 400;
+    height: 45px;
+    }
+    & .MuiOutlinedInput-notchedOutline {
+    border: none;
+    }
+    &:hover{
+        background-color: #f7f8fa;
+    }
+    &:focus-within {
+        border: 1px solid #000000;
+    }
+`
+export const StyledInputPw = styled(FilledInput)`
+    width: 360px;
+    height: 45px;
+    background-color: #F9FAFB !important;
+    border-radius: 5px;
+    position: relative;
+    & .MuiFilledInput-input{
+        font-size: 14px !important;
+        font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+        font-weight: 400;
+        padding: 0;
+        padding: 16.5px 14px;
+    }
+    &:hover{
+        background-color: #f7f8fa;
+    }
+    &:focus-within {
+        border: 1px solid #000000;
+    }
+    &:before{
+        border: none !important;
+    }
+    &:after{
+        border: none !important;
+    }
+`
+export const PwButton = styled(IconButton)`
+    color: #B2B3B4 !important;
+    padding: 16.5px 14px !important;
+    height: 35px;
+    width: 35px;
+    margin: 0 !important;
+    & .MuiSvgIcon-root{
+        font-size: 20px !important;
+    }
+    & .css-1yq5fb3-MuiButtonBase-root-MuiIconButton-root:hover{
+        background-color: transparent !important;
+    }
+` 
+export const ErrorMsg = styled.div`
+    color: #d32f2f;
+        font-family: "Roboto","Helvetica","Arial",sans-serif;
+    font-weight: 400;
+    font-size: 0.75rem;
+    line-height: 1.66;
+    letter-spacing: 0.03333em;
+    text-align: left;
+    margin-top: 3px;
+    margin-right: 14px;
+    margin-bottom: 0;
+    margin-left: 14px;
+`
+export const CheckStyle = styled.div`
+    background-color: #F9FAFB;
+    text-align: left;
+    padding: 16px 14px;
+    color: #777D82;
+    & .MuiCheckbox-root{
+        color: #C7CED6;
+    }
+    & .css-ahj2mt-MuiTypography-root{
+        font-size: 13px !important;
+        font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+    }
+    & .css-12wnr2w-MuiButtonBase-root-MuiCheckbox-root.Mui-checked, .css-12wnr2w-MuiButtonBase-root-MuiCheckbox-root.MuiCheckbox-indeterminate{
+        color: #000;
+    }
+`
+
+export const FormControlStyle = styled(FormControlLabel)`
+    & .MuiTypography-root{
+    font-size: 13px !important;
+    font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+    font-weight: 300;
+    color: #777D82;
+    }
+    & .MuiCheckbox-root{
+        color: #C7CED6;
+    }
+    & .MuiCheckbox-root.Mui-checked, .MuiCheckbox-indeterminate{
+        color: #000;
+    }
+    & .css-12wnr2w-MuiButtonBase-root-MuiCheckbox-root.Mui-checked, .css-12wnr2w-MuiButtonBase-root-MuiCheckbox-root.MuiCheckbox-indeterminate{
+        color: #000
+    }
+`
+export const SubmitBtn = styled.button`
+    width: 360px;
+    height: 50px;
+    background-color: #C2C4C7;
+    border: none;
+    font-family: ${({ isEnglish }) => (isEnglish ? 'Montserrat' : 'SUITE-Regular')};
+    color: #fff;
+    font-size: 16px;
+    font-weight: 300;
+    border-radius: 5px;
+`
