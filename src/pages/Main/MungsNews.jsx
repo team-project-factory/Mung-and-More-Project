f7f4220e3263ddfa5d51dc08dded2b99dabc77ea
Kdddru <dhk9817@gmail.com>
2023-06-09 13:13:18 +0900

    0609 수정

--- src/pages/Main/MungsNews.jsx
+++ src/pages/Main/MungsNews.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { MungsNewsComp } from '../../components/main/community/mungsNews/MungsNewsComp'
+import { Nav } from '../../layout/Nav'
 
 export const MungsNews = () => {
   return (
     <div>
+      <div style={{width : `100%`, position :`relative`, top:'50px'}}>
+        <Nav/>
+      </div>
       <MungsNewsComp/>
     </div>
   )
