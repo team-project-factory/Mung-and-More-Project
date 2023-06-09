bceb17cbb0931d9413832106a2e5a46677dd6c90
Kdddru <dhk9817@gmail.com>
2023-06-09 12:49:40 +0900

    0609 멍스2개 추가

---
+++ src/pages/Main/MungsNews.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MungsNewsComp } from '../../components/main/community/mungsNews/MungsNewsComp'
+
+export const MungsNews = () => {
+  return (
+    <div>
+      <MungsNewsComp/>
+    </div>
+  )
+}
+
---
+++ src/pages/Main/Mungstagram.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MungstagramComp } from '../../components/main/community/mungstagram/MungstagramComp'
+
+export const Mungstagram = () => {
+  return (
+    <div>
+      <MungstagramComp/>
+    </div>
+  )
+}
