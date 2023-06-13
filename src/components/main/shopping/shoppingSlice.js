7d3d299c519a82e3337f573c5337085be304f822
Kdddru <dhk9817@gmail.com>
2023-06-13 17:25:59 +0900

    0613 하트로 변경만 가능

---
+++ src/components/main/shopping/shoppingSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+export const shoppingSlice = createSlice({
+  name : 'shopping',
+  initialState : null,
+  reducers : {
+    
+  }
+})
+
+export const getItemData = ()=>(dispatch)=>{
+
+} 
+
+
+export const{} = shoppingSlice.actions;
+export default shoppingSlice.reducer
