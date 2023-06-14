a46fe02ce4485bc5ad5861adb320bb39db36b09f
Jiyeon <tjdwldus95@naver.com>
2023-06-14 12:54:01 +0900

    0614 JY Event Page

--- src/components/main/notice/EventListComp.jsx
+++ src/components/main/notice/EventListComp.jsx
@@ -1,41 +1,79 @@
-import React, { useState } from 'react';
-import './eventListComp.css'
+import React from "react";
 
-function EventListComp() {
-  const [imageIndex, setImageIndex] = useState(0);
+// 이미지 슬라이더에 필요한 import
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
-  // 아래 배열 묶음은 한 화면에 출력됨(3장씩)
-  const images = [
-    ["./img/post.jpg","./img/post1.jpg","./img/post2.jpg"],
-    ["./img/login.png","./img/login2.png","./img/login3.png"],
-    ["./img/login4.png","./img/login5.png","./img/login6.png"]
-  ];
+// css import
+import "./eventListComp.css";
 
-  function btnPrev() {
-    setImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  }
-  
-  function btnNext() {
-    setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  }
-
+export default function App() {
   return (
-    <div className='event-slide'>
-      {images[imageIndex].map((image, index) => (
-        <img
-          key={index}
-          className='event-slide-img'
-          src={image}
-          alt={`Image ${imageIndex + 1}`}
-        />
-      ))}
-      <div className='slide-btn'>
-        <button className='prev-button' onClick={btnPrev}>{'<'}</button>
-        <button className='next-button' onClick={btnNext}>{'>'}</button>
-      </div>
+    <div className="swiper-container">
+      <Swiper
+        slidesPerView={4}
+        centeredSlides={true}
+        spaceBetween={300}
+        grabCursor={true}
+        pagination={{
+          clickable: true,
+          el: ".swiper-pagination",
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="slide-image">
+            <img src="./img/event-img1.png" alt="Slide 1" />
+            <div className="image-overlay">
+              <p>이벤트 상세 설명</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-image">
+            <img src="./img/post.jpg" alt="Slide 2" />
+            <div className="image-overlay">
+              <p>Text 1</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-image">
+            <img src="./img/post.jpg" alt="Slide 3" />
+            <div className="image-overlay">
+              <p>Text 1</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-image">
+            <img src="./img/post.jpg" alt="Slide 4" />
+            <div className="image-overlay">
+              <p>Text 1</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-image">
+            <img src="./img/post.jpg" alt="Slide 5" />
+            <div className="image-overlay">
+              <p>Text 1</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-image">
+            <img src="./img/post.jpg" alt="Slide 6" />
+            <div className="image-overlay">
+              <p>Text 1</p>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div className="swiper-pagination"></div>
     </div>
   );
 }
-
-export default EventListComp;
-
