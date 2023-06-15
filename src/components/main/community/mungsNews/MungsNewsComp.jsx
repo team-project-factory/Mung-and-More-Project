387c7740244cb4427a2482ce9169f07b113e657b
Kdddru <dhk9817@gmail.com>
2023-06-15 15:44:26 +0900

    0615머지

--- src/components/main/community/mungsNews/MungsNewsComp.jsx
+++ src/components/main/community/mungsNews/MungsNewsComp.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+
+//css
 import style from './mungsnewscomp.module.scss'
-import { Outlet } from 'react-router-dom'
+
+//파이어베이스
+import { db } from '../../../../data/firebase';
+import { collection, getDocs } from "firebase/firestore";
 
 
 export const MungsNewsComp = () => {
+  const [news, setNews] = useState('');
+
+
+  useEffect(()=>{
+    const getData = async() =>{
+      const querySnapshot = await getDocs(collection(db, "News"));
+      const newsList = [];
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().news}`);
+        const data =doc.data().news;
+        newsList.push({
+          id : doc.id,
+          ...data
+        });
+      });
+      setNews(newsList);     
+    }
+    getData();
+  },[]);
+
+  
+
   return (
     <div className={style.mungsList}>
       <div className={style.mungsList_menu}>
-        <h1>Mung's news</h1>
+        {news && news.map((n)=>(
+          <Link to={`/mungsnews/${n.id}`}><h1>{n.title}</h1></Link>
+        ))}
       </div>
-      <Outlet/>
+      <Outlet context={news}/>
     </div>
   )
 }
--- src/components/main/community/mungsNews/MungsNewsContents.jsx
+++ src/components/main/community/mungsNews/MungsNewsContents.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useOutletContext, useParams } from 'react-router-dom'
+
+
 
 export const MungsNewsContents = () => {
+  const [newsList, setNewsList] = useState();
+  
+  const Id = useParams();
+  
+  const news = useOutletContext();
+
+
+
+  useEffect(()=>{
+    if(news){
+      const NewList = news.filter((n)=>(
+        n.id == Id.name
+      ))
+      setNewsList(NewList);
+    }
+  },[])
+
+
   return (
-    <div>News</div>
+    <div>
+      {newsList && newsList.map((n)=>(
+        <div>
+          <h1>{n.title}</h1>
+          <div>
+            {n.contents && n.contents.map((c)=>(
+              <ul>
+                <li>
+                  {c.title}
+                </li>
+              </ul>
+              ))}
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
