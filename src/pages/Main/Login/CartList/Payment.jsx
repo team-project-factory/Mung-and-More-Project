98cf2a9f17f1c59d78fc22cc017845ebff3e0b3b
Kdddru <125441996+Kdddru@users.noreply.github.com>
2023-06-16 16:04:31 +0900

    0616 멍스뉴스 수정

--- src/pages/Main/Login/CartList/Payment.jsx
+++ src/pages/Main/Login/CartList/Payment.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { PaymentComp } from '../../../../components/main/login/cartlist/payment/PaymentComp'
+import { Nav } from '../../../../layout/Nav'
 
 export const Payment = () => {
   return (
     <div>
       <div style={{ width: `100%`, position: `relative`, top: '50px' }}>
-        <Nav />
+        <Nav/>
       </div>
       <PaymentComp/>
     </div>
