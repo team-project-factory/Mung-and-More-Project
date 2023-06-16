db53bf6973549cfca59c4d7edbe5cadb35de9bc8
Kdddru <dhk9817@gmail.com>
2023-06-16 11:07:20 +0900

    0616

--- src/pages/Main/Login/CartList/Payment.jsx
+++ src/pages/Main/Login/CartList/Payment.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import { PaymentComp } from '../../../../components/main/login/cartlist/payment/PaymentComp'
-import { Nav } from '../../../../layout/Nav'
 
 export const Payment = () => {
   return (
     <div>
-      <div style={{ width: `100%`, position: `relative`, top: '50px' }}>
-        <Nav />
-      </div>
       <PaymentComp/>
     </div>
   )
