54e6a90088f4c20cefc0cecc4ef8124743c7edcf
Kdddru <dhk9817@gmail.com>
2023-06-16 11:07:48 +0900

    0616 payment에 nav바추가

--- src/pages/Main/Login/CartList/Payment.jsx
+++ src/pages/Main/Login/CartList/Payment.jsx
@@ -4,6 +4,9 @@ import { PaymentComp } from '../../../../components/main/login/cartlist/payment/
 export const Payment = () => {
   return (
     <div>
+      <div style={{ width: `100%`, position: `relative`, top: '50px' }}>
+        <Nav />
+      </div>
       <PaymentComp/>
     </div>
   )
