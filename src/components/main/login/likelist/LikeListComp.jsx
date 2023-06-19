54a86908b6ccd7a89d06a98e305756c7b2019ac5
Jiyeon <tjdwldus95@naver.com>
2023-06-19 16:50:47 +0900

    0619 JY

--- src/components/main/login/likelist/LikeListComp.jsx
+++ src/components/main/login/likelist/LikeListComp.jsx
@@ -1,70 +1,75 @@
-//파이어베이스
+// firebase
 import { auth, db } from "../../../../data/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+// react
+import React, { useEffect, useState } from "react";
 
-
-import React, { useEffect, useState }  from 'react'
+// scss
+import style from "./likeListComp.module.scss";
 
 export const LikeListComp = () => {
-//user UID 담을 state
-const [userUID,setUserUID] = useState('');
-//cartList 담을 state
-const [likeList, setCartList] = useState('');
+  // user UID 담을 state
+  const [userUID, setUserUID] = useState("");
 
-//유저 데이터 들고오기
-const getUserData = () => {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-      setUserUID(uid);
-      // ...
+  // cartList 담을 state
+  const [likeList, setCartList] = useState("");
+
+  // 유저 데이터 들고오기
+  const getUserData = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        setUserUID(uid);
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+  };
+
+  // 화면 출력하자마자 유저데이터 들고오기
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  // 유저가 가지고 있는 데이터 들고오기
+  const getData = async () => {
+    const docRef = doc(db, "users", userUID);
+    const docSnap = await getDoc(docRef);
+    const cart = docSnap.data().likeList;
+
+    if (docSnap.exists()) {
+      console.log("Document data:", cart);
+      setCartList(cart);
     } else {
-      // User is signed out
-      // ...
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
     }
-  });
-}
-//화면 출력하자마자 유저데이터 들고오기
-useEffect(()=>{
-  getUserData();
-},[])
-//유저가지고 있는 데이터 들고오기
-const getData = async() =>{
-  const docRef = doc(db, "users", userUID);
-  const docSnap = await getDoc(docRef);
-  const cart = docSnap.data().likeList;
-  
-  if (docSnap.exists()) {
-    console.log("Document data:", cart);
-    setCartList(cart);
-  } else {
-    // docSnap.data() will be undefined in this case
-    console.log("No such document!");
-  }
-}  
+  };
 
-useEffect(()=>{
-  if(userUID){
-    getData();
-  }
-},[userUID]);
+  useEffect(() => {
+    if (userUID) {
+      getData();
+    }
+  }, [userUID]);
 
-if(likeList){
-  console.log(likeList);
-}
+  if (likeList) {
+    console.log(likeList);
+  }
 
   return (
-    <div>
-      LikeListComp
-      <ul>
-        {likeList && likeList.map((item)=>(
-          <li>{item.name}</li>
-        ))}
-      </ul>
+    <div className={style.LikeListComp}>
+      <div className={style.Layout}>
+        <div className={style.LikeListBox}>
+          <h1>My Like List</h1>
+          <ul>{likeList && likeList.map((item) => <li>{item.name}</li>)}</ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
