dcfb4a0252205f9ea93cb29d8c43b8adf1498359
syzzny <son115502@gmail.com>
2023-06-22 14:54:00 +0900

    add

--- src/components/main/login/mypage/mypost/PostComp.jsx
+++ src/components/main/login/mypage/mypost/PostComp.jsx
@@ -1,31 +1,51 @@
-import React,{ useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Nav } from '../../../../../layout/Nav';
-
-//파이어베이스
-import { db } from '../../../../../data/firebase';
+import { db, storage } from '../../../../../data/firebase';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 import {
     Wrap, Title, ContentWrap, PostWrap, Post, PostInner, PostInfo, Goto, Date, PostTitle
-} from './styles/PostStyleComp'
+} from './styles/PostStyleComp';
 
 export default function PostComp() {
-
     const [email, setEmail] = useState('');
-    const [isLoggedIn, setIsLoggedIn] = useState(false); // 추가: 로그인 상태
-
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [postList, setPostList] = useState([]);
+    
     useEffect(() => {
-        // 추가: 로그인 상태 변경 감지
         const auth = getAuth();
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 setIsLoggedIn(true);
                 setEmail(user.email || '');
+                getData(user.uid);
             } else {
                 setIsLoggedIn(false);
             }
         });
     }, []);
+
+    const getData = (userId) => {
+        const querySnapshotPromise = getDocs(query(collection(db, 'post'), where('uid', '==', userId)));
+
+        querySnapshotPromise.then((querySnapshot) => {
+            const newList = [];
+            querySnapshot.forEach((doc) => {
+                const data = doc.data();
+
+                newList.push({
+                    id: doc.id,
+                    imageIndex: 0,
+                    ...data
+                });
+            });
+
+            setPostList(newList);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
 
     return (
         <Wrap>
@@ -35,30 +55,21 @@ export default function PostComp() {
             <Title>작성한 게시물</Title>
             <ContentWrap>
                 <PostWrap>
-                    <Post>
-                        <PostInner>
-                            <img src="./img/board1.png" alt="" />
-                            <PostInfo>
-                                <Date>00.00.23</Date>
-                                <PostTitle>Post Title</PostTitle>
-                                <p>{email}</p>
-                                <Goto>Go To</Goto>
-                            </PostInfo>
-                        </PostInner>
-                    </Post>
-                    <Post>
-                        <PostInner>
-                            <img src="./img/board1.png" alt="" />
-                            <PostInfo>
-                                <Date>00.00.23</Date>
-                                <PostTitle>Post Title</PostTitle>
-                                <p>{email}</p>
-                                <Goto>Go To</Goto>
-                            </PostInfo>
-                        </PostInner>
-                    </Post>
+                    {postList.map((post) => (
+                        <Post key={post.id}>
+                            <PostInner>
+                                <img src={post.photo} alt="" />
+                                <PostInfo>
+                                    <Date>{post.date}</Date>
+                                    <PostTitle>{post.title}</PostTitle>
+                                    <p>{email}</p>
+                                    <Goto>Go To</Goto>
+                                </PostInfo>
+                            </PostInner>
+                        </Post>
+                    ))}
                 </PostWrap>
             </ContentWrap>
         </Wrap>
-    )
+    );
 }
--- src/components/main/login/mypage/orderlist/OrderListComp.jsx
+++ src/components/main/login/mypage/orderlist/OrderListComp.jsx
@@ -101,7 +101,6 @@ export const OrderListComp = () => {
                   ))
                 }
 
-
               </div>
             ))}
         </OrderWrap>
