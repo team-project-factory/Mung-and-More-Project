4ef79ab01b2af32c78b9cf35c6d5062719e37fd1
syzzny <son115502@gmail.com>
2023-06-22 15:38:38 +0900

    add

--- src/components/main/login/mypage/mypost/PostComp.jsx
+++ src/components/main/login/mypage/mypost/PostComp.jsx
@@ -1,50 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { Nav } from '../../../../../layout/Nav';
+
+//파이어베이스
 import { db, storage } from '../../../../../data/firebase';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { getAuth, signOut, onAuthStateChanged, updateProfile, updatePassword, deleteUser } from "firebase/auth";
+import { doc, deleteDoc, collection, getDocs } from "firebase/firestore";
+import { useDispatch } from 'react-redux';
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 
 import {
     Wrap, Title, ContentWrap, PostWrap, Post, PostInner, PostInfo, Goto, Date, PostTitle
-} from './styles/PostStyleComp';
+} from './styles/PostStyleComp'
 
 export default function PostComp() {
+
     const [email, setEmail] = useState('');
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // 추가: 로그인 상태
+    // 게시물 리스트 상태 추가
     const [postList, setPostList] = useState([]);
-    
+
     useEffect(() => {
+        // 추가: 로그인 상태 변경 감지
         const auth = getAuth();
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 setIsLoggedIn(true);
                 setEmail(user.email || '');
+                // 데이터 가져오기 함수 호출
                 getData(user.uid);
             } else {
                 setIsLoggedIn(false);
             }
         });
+        
     }, []);
 
-    const getData = (userId) => {
-        const querySnapshotPromise = getDocs(query(collection(db, 'post'), where('uid', '==', userId)));
-
-        querySnapshotPromise.then((querySnapshot) => {
-            const newList = [];
-            querySnapshot.forEach((doc) => {
-                const data = doc.data();
+    const getData = async (uid) => {
+        const querySnapshot = await getDocs(collection(db, 'post'));
+        const newList = [];
+        querySnapshot.forEach((doc) => {
+            const data = doc.data().post;
+            if (data.uid == uid) {
+                // 이미지 참조 생성
+                const imageRef = ref(storage, data.imagePath);
+                // 이미지 다운로드 URL
 
                 newList.push({
                     id: doc.id,
                     imageIndex: 0,
                     ...data
                 });
-            });
+            }
 
-            setPostList(newList);
-        }).catch((error) => {
-            console.log(error);
         });
+        // 게시물 리스트 상태 업데이트
+        setPostList(newList);
     }
 
     return (
@@ -58,9 +68,9 @@ export default function PostComp() {
                     {postList.map((post) => (
                         <Post key={post.id}>
                             <PostInner>
-                                <img src={post.photo} alt="" />
+                                <img src={post.images&&post.images[0]} alt="" />
                                 <PostInfo>
-                                    <Date>{post.date}</Date>
+                                    <Date>00.00.23</Date>
                                     <PostTitle>{post.title}</PostTitle>
                                     <p>{email}</p>
                                     <Goto>Go To</Goto>
@@ -71,5 +81,5 @@ export default function PostComp() {
                 </PostWrap>
             </ContentWrap>
         </Wrap>
-    );
+    )
 }
