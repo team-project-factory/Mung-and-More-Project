cc59779c2c701e4986877804006610cfbe698c08
K K <koke0415@naver.com>
2023-06-26 16:56:42 +0900

    0626 메인 TOP버튼

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MainComp1 } from '../../components/main/MainComp1'
 import { MainComp2 } from '../../components/main/MainComp2'
 import { MainComp3 } from '../../components/main/MainComp3'
@@ -6,8 +6,28 @@ import { MainComp4 } from '../../components/main/MainComp4'
 import { MainComp5 } from '../../components/main/MainComp5'
 import { MainComp6 } from '../../components/main/MainComp6'
 import { Nav } from '../../layout/Nav'
+import './Main.css'
 
 export const Main = () => {
+
+  // 아래는 스크롤 Y축 1300 이상일 때만 TOP버튼이 나오게 하는 설정
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+    
+    const showTopButton = scrollY >= 1300;
+
+
   return (
     <div style={{backgroundColor: `#C2DCF4`}}>
       <div>
@@ -21,6 +41,9 @@ export const Main = () => {
       <MainComp4/>
       <MainComp5/>
       <MainComp6/>
+      {showTopButton && 
+      <a href="#" className='page-top'>TOP</a>
+      }
     </div>
   )
 }
