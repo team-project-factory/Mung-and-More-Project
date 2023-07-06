4b73ebf7f699bf17e415a78f7d9b7812f8d2401f
Kdddru <dhk9817@gmail.com>
2023-07-06 12:01:57 +0900

    배포

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <App />
         </HashRouter>
     </Provider>
